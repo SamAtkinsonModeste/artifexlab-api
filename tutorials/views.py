22d9ba08cfea491eebe4fd27e94fe3dffc01ce25
Sam Atkinson Modeste <sammodeste@yahoo.com>
2025-05-27 17:36:55 +0100

    feat: add view - user can access tutorials via API

--- tutorials/views.py
+++ tutorials/views.py
@@ -1,3 +1,61 @@
-from django.shortcuts import render
+from django.db.models import Count
+from rest_framework import generics, permissions, filters
+from django_filters.rest_framework import DjangoFilterBackend
+from artlab_api.permissions import IsOwnerOrReadOnly
+from .models import Tutorial
+from .serializers import TutorialSerializer
 
-# Create your views here.
+
+class TutorialList(generics.ListCreateAPIView):
+    """
+    List tutorials or create a tutorial if logged in
+    The perform_create method associates the tutorial with the logged in user.
+    """
+
+    serializer_class = TutorialSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Tutorial.objects.annotate(
+        tutorial_likes_count=Count("likes", distinct=True),
+        tutorial_comments_count=Count("comment", distinct=True),
+    ).order_by("-created_at")
+    filter_backends = [
+        filters.OrderingFilter,
+        filters.SearchFilter,
+        DjangoFilterBackend,
+    ]
+
+    filterset_fields = [
+        #REVIEW - for related names values in the models
+        "owner__followed__owner__profile",
+        "tutorial_likes__owner__profile",
+        "owner__profile",
+    ]
+
+    search_fields = [
+        "owner__username",
+        "title",
+    ]
+
+    ordering_fields = [
+        #REVIEW - for related name values in the models
+        "tutorial_comments_count",
+        "tutorial_likes_count",
+        "tutorial_likes__created_at",
+    ]
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class TutorialDetail(generics.RetrieveUpdateDestroyAPIView):
+    """
+    Retrieve a post and edit or delete it if you own it.
+    """
+
+    serializer_class = TutorialSerializer
+    permission_classes = [IsOwnerOrReadOnly]
+    queryset = Tutorial.objects.annotate(
+        #REVIEW - for related names created in the like and comments models
+        tutorial_likes_count=Count("likes", distinct=True),
+        tutorial_comments_count=Count("comments", distinct=True),
+    ).order_by("-created_at")
