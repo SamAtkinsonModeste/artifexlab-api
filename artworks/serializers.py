14d8f4fd04c0bc72edb444699e1ae303830451f0
Sam Atkinson Modeste <sammodeste@yahoo.com>
2025-05-29 23:56:31 +0100

    feat: imported new likeArtwork model

--- artworks/serializers.py
+++ artworks/serializers.py
@@ -1,16 +1,18 @@
 from rest_framework import serializers
 from .models import Artwork
-from likes.models import Like
+from likes.models import LikeArtwork
 
 
 class ArtworkSerializer(serializers.ModelSerializer):
-    owner = serializers.ReadOnlyField(source="owner.username")
-    is_owner = serializers.SerializerMethodField()
-    profile_id = serializers.ReadOnlyField(source="owner.profile.id")
-    profile_image = serializers.ReadOnlyField(source="owner.profile.image.url")
-    liked_id = serializers.SerializerMethodField()
-    likes_count = serializers.ReadOnlyField()
-    comments_count = serializers.ReadOnlyField()
+    artwork_owner = serializers.ReadOnlyField(source="owner.username")
+    is_artwork_owner = serializers.SerializerMethodField()
+    artist_profile_id = serializers.ReadOnlyField(source="owner.profile.id")
+    artist_profile_image = serializers.ReadOnlyField(source="owner.profile.image.url")
+    artwork_title = serializers.ReadOnlyField(source="title")
+    artwork_description = serializers.ReadOnlyField(source="description")
+    artwork_liked_id = serializers.SerializerMethodField()
+    artwork_likes_count = serializers.ReadOnlyField()
+    # artwork_comments_count = serializers.ReadOnlyField()
 
     def validate_image(self, value):
         """
@@ -27,32 +29,31 @@ class ArtworkSerializer(serializers.ModelSerializer):
             raise serializers.ValidationError("Image height cannot exceed 4096px!")
         return value
 
-    def get_is_owner(self, obj):
+    def get_is_artwork_owner(self, obj):
         request = self.context["request"]
         return request.user == obj.owner
 
-    def get_liked_id(self, obj):
+    def get_artwork_liked_id(self, obj):
         user = self.context["request"].user
         if user.is_authenticated:
-            liked = Like.objects.filter(owner=user, artwork=obj).first()
-            return liked.id if liked else None
+            artwork_liked = LikeArtwork.filter(owner=user, artwork=obj).first()
+            return artwork_liked.id if artwork_liked else None
         return None
 
     class Meta:
         model = Artwork
         fields = [
             "id",
-            "owner",
-            "is_owner",
-            "profile_id",
-            "profile_image",
+            "artwork_owner",
+            "is_artwork_owner",
+            "artist_profile_id",
+            "artist_profile_image",
             "created_at",
             "updated_at",
-            "title",
-            "description",
+            "artwork_title",
+            "artwork_description",
             "image",
-            "image_filter_choices",
-              "liked_id",
-             "comments_count",
-            "likes_count",
+              "artwork_liked_id",
+            #  "artwork_comments_count",
+            "artwork_likes_count",
         ]
