d3f940dcb359911c21261fb1dfecab90c9a207de
Sam Atkinson Modeste <sammodeste@yahoo.com>
2025-05-29 17:44:45 +0100

    refactor:views-likes create base list & detail views
    Used the base list & detail views on the Artwork serializer.

--- likes/views.py
+++ likes/views.py
@@ -1,27 +1,57 @@
 from rest_framework import generics, permissions
 from artlab_api.permissions import IsOwnerOrReadOnly
-from likes.models import Like
-from likes.serializers import LikeSerializer
+from likes.models import LikeArtwork, LikeTutorial
+from likes.serializers import LikeArtworkSerializer, LikeTutorialSerializer
 
-
-class LikeList(generics.ListCreateAPIView):
+class BaseLikeList(generics.ListCreateAPIView):
     """
     List likes or create a like if logged in.
     """
-
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
-    serializer_class = LikeSerializer
-    queryset = Like.objects.all()
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
 
 
-class LikeDetail(generics.RetrieveDestroyAPIView):
+class BaseLikeDetail(generics.RetrieveDestroyAPIView):
     """
     Retrieve a like or delete it by id if you own it.
     """
-
     permission_classes = [IsOwnerOrReadOnly]
-    serializer_class = LikeSerializer
-    queryset = Like.objects.all()
+
+
+class LikeArtworkList(BaseLikeList):
+    serializer_class = LikeArtworkSerializer
+    queryset = LikeArtwork.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+class LikeArtworkDetail(BaseLikeDetail):
+    """
+    Retrieve a like or delete it by id if you own it.
+    """
+    serializer_class = LikeArtworkSerializer
+    queryset = LikeArtwork.objects.all()
+
+# class LikeList(generics.ListCreateAPIView):
+#     """
+#     List likes or create a like if logged in.
+#     """
+
+#     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+#     serializer_class = LikeSerializer
+#     queryset = Like.objects.all()
+
+#     def perform_create(self, serializer):
+#         serializer.save(owner=self.request.user)
+
+
+# class LikeDetail(generics.RetrieveDestroyAPIView):
+#     """
+#     Retrieve a like or delete it by id if you own it.
+#     """
+
+#     permission_classes = [IsOwnerOrReadOnly]
+#     serializer_class = LikeSerializer
+#     queryset = Like.objects.all()
