522583dc00b2bf5b2e109c6fadd7010e3227590d
Sam Atkinson Modeste <sammodeste@yahoo.com>
2025-05-29 16:55:28 +0100

    refactor:serializers-likes created base & 2 inherited it

--- likes/serializers.py
+++ likes/serializers.py
@@ -1,9 +1,9 @@
 from django.db import IntegrityError
 from rest_framework import serializers
-from .models import Like
+from .models import LikeArtwork, LikeTutorial
 
 
-class LikeSerializer(serializers.ModelSerializer):
+class LikeBaseSerializer(serializers.ModelSerializer):
     """
     Serializer for the Like model
     Adds three extra fields when returning a list of Like instances
@@ -12,13 +12,24 @@ class LikeSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source="owner.username")
 
     class Meta:
-        model = Like
-        fields = ["id", "owner", "artwork", "created_at"]
+        fields = ["id", "owner", "created_at"]
 
     def create(self, validated_data):
         try:
             return super().create(validated_data)
         except IntegrityError:
             raise serializers.ValidationError(
-                {"detail": "You have already liked this digital artwork."}
+                {"detail": "You have already liked this!"}
             )
+
+class LikeArtworkSerializer(LikeBaseSerializer, serializers.ModelSerializer):
+    class Meta:
+        model = LikeArtwork
+        fields = LikeBaseSerializer.Meta.fields + ["artwork"]
+
+
+
+class LikeTutorialSerializer(LikeBaseSerializer, serializers.ModelSerializer):
+    class Meta:
+        model = LikeTutorial
+        fields = LikeBaseSerializer.Meta.fields + ["tutorial"]
