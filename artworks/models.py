6ca35478166aa940848bc5c69703494e4db4b54e
Sam Atkinson Modeste <sammodeste@yahoo.com>
2025-05-29 18:56:13 +0100

    removed:model- filter_field

--- artworks/models.py
+++ artworks/models.py
@@ -11,22 +11,7 @@ class Artwork(models.Model):
     Default image set so that we can always reference image.url.
     """
 
-    IMAGE_FILTER_CHOICES = [
-        ("_1977", "1977"),
-        ("brannan", "Brannan"),
-        ("earlybird", "Earlybird"),
-        ("hudson", "Hudson"),
-        ("inkwell", "Inkwell"),
-        ("lofi", "Lo-Fi"),
-        ("kelvin", "Kelvin"),
-        ("normal", "Normal"),
-        ("nashville", "Nashville"),
-        ("rise", "Rise"),
-        ("toaster", "Toaster"),
-        ("valencia", "Valencia"),
-        ("walden", "Walden"),
-        ("xpro2", "X-pro II"),
-    ]
+
     owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="artworks")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -35,9 +20,7 @@ class Artwork(models.Model):
     image = models.ImageField(
         upload_to="images/", default="../default_post_rgq6aq", blank=True
     )
-    image_filter_choices = models.CharField(
-        max_length=32, choices=IMAGE_FILTER_CHOICES, default="normal"
-    )
+
 
     class Meta:
         ordering = ["-created_at"]
