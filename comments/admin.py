bb1635f06a24b709a373ddda9cca0634cce09585
Sam Atkinson Modeste <sammodeste@yahoo.com>
2025-05-30 14:01:41 +0100

    refactor:admin - registered 2 new models
    The 2 new models are CommentArtwork & CommentTutorial

--- comments/admin.py
+++ comments/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Comment
+from .models import CommentArtwork, CommentTutorial
 
 # Register your models here.
-admin.site.register(Comment)
+admin.site.register(CommentArtwork)
+admin.site.register(CommentTutorial)
