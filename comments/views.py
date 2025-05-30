246c4ac3345e503bf4e53a74ba6a58fea7bb61e9
Sam Atkinson Modeste <sammodeste@yahoo.com>
2025-05-30 15:31:29 +0100

    refactor:views - created base list & detail views
    CommentArtwork & CommentTutorial models and serializers inherited from both list & detail base views

--- comments/views.py
+++ comments/views.py
@@ -1,30 +1,46 @@
 from rest_framework import generics, permissions
 from django_filters.rest_framework import DjangoFilterBackend
 from artlab_api.permissions import IsOwnerOrReadOnly
-from .models import Comment
-from .serializers import CommentSerializer, CommentDetailSerializer
+from .models import CommentArtwork, CommentTutorial
+from .serializers import CommentArtworkSerializer, CommentArtworkDetailSerializer, CommentTutorialSerializer, CommentTutorialDetailSerializer
 
 
-class CommentList(generics.ListCreateAPIView):
+class BaseCommentList(generics.ListCreateAPIView):
     """
-    List comments or create a comment if logged in.
+    BaseCommentList to enable inheritance by all list views.
+    DRY
     """
-
-    serializer_class = CommentSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
-    queryset = Comment.objects.all()
-    filter_backends = [DjangoFilterBackend]
-    filterset_fields = ["post"]
-
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
 
 
-class CommentDetail(generics.RetrieveUpdateDestroyAPIView):
+class BaseCommentDetail(generics.RetrieveUpdateDestroyAPIView):
     """
-    Retrieve a comment, or update or delete it by id if you own it.
+    BaseCommentDetail to enable inheritance by all views.
+    DRY
     """
-
     permission_classes = [IsOwnerOrReadOnly]
-    serializer_class = CommentDetailSerializer
-    queryset = Comment.objects.all()
+
+class CommentArtworkList(BaseCommentList):
+    serializer_class = CommentArtworkSerializer
+    queryset = CommentArtwork.objects.all()
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ["artwork"]
+
+
+class CommentTutorialList(BaseCommentList):
+    serializer_class = CommentTutorialSerializer
+    queryset = CommentTutorial.objects.all()
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ["tutorial"]
+
+
+class CommentArtworkDetail(BaseCommentDetail):
+    serializer_class = CommentArtworkDetailSerializer
+    queryset = CommentArtwork.objects.all()
+
+
+class CommentTutorialDetail(BaseCommentDetail):
+    serializer_class = CommentTutorialDetailSerializer
+    queryset = CommentTutorial.objects.all()
