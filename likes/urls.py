3ed8abf54ae9779d9d140e6cd007eabc2c62d801
Sam Atkinson Modeste <sammodeste@yahoo.com>
2025-05-30 12:43:42 +0100

    refactor: urls to all new Like models

--- likes/urls.py
+++ likes/urls.py
@@ -4,5 +4,7 @@ from likes import views
 urlpatterns = [
     path("likesArtwork/", views.LikeArtworkList.as_view()),
     path("likesArtwork/<int:pk>/", views.LikeArtworkDetail.as_view()),
+    path("likesTutorial/",views.LikeTutorialList.as_view()),
+    path("likesTutorial/<int:pk>/", views.LikeTutorialDetail.as_view()),
 
 ]
