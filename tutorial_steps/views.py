7f614a8cb2ce880b5c50027737e60baf3fc757ec
Sam Atkinson Modeste <sammodeste@yahoo.com>
2025-05-30 23:21:17 +0100

    feat: views - List & Details done for Tutorial Steps

--- tutorial_steps/views.py
+++ tutorial_steps/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from artlab_api.permissions import IsOwnerOrReadOnly
+from .models import TutorialSteps
+from .serializers import TutorialStepsSerializer
 
-# Create your views here.
+
+class TutorialStepsList(generics.ListCreateAPIView):
+    """
+    List of Tutorial steps or create a new step
+    """
+
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    serializer_class = TutorialStepsSerializer
+    queryset = TutorialSteps.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+
+class TutorialStepsDetail(generics.RetrieveUpdateDestroyAPIView):
+    """
+    Retrieve, update, or delete a tutorial step
+    """
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    serializer_class = TutorialStepsSerializer
+    queryset = TutorialSteps.objects.all()
