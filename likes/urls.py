d4d980af33aba03c67dfa03e6323e7ad11239c27
Sam Atkinson Modeste <sammodeste@yahoo.com>
2025-05-30 15:48:24 +0100

    refactor:urls hyhenated the route path text

--- likes/urls.py
+++ likes/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from likes import views
 
 urlpatterns = [
-    path("likesArtwork/", views.LikeArtworkList.as_view()),
-    path("likesArtwork/<int:pk>/", views.LikeArtworkDetail.as_view()),
-    path("likesTutorial/",views.LikeTutorialList.as_view()),
-    path("likesTutorial/<int:pk>/", views.LikeTutorialDetail.as_view()),
+    path("artwork-likes/", views.LikeArtworkList.as_view()),
+    path("artwork-likes/<int:pk>/", views.LikeArtworkDetail.as_view()),
+    path("tutorial-likes/",views.LikeTutorialList.as_view()),
+    path("tutorial-likes/<int:pk>/", views.LikeTutorialDetail.as_view()),
 
 ]
