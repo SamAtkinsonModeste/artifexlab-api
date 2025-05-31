03eb3ce24f1120c897ee4a0f6b478450c8e2c98c
Sam Atkinson Modeste <sammodeste@yahoo.com>
2025-05-31 23:59:32 +0100

    feat: urls - additional tutorials-setps,attempts,feedback added

--- tutorials/urls.py
+++ tutorials/urls.py
@@ -2,6 +2,12 @@ from django.urls import path
 from tutorials import views
 
 urlpatterns = [
-    path("tutorials/", views.TutorialList.as_view()),
+    path("tutorials/", views.TutorialsList.as_view()),
     path("tutorials/<int:pk>/", views.TutorialDetail.as_view()),
+    path("tutorial-steps/", views.TutorialStepsList.as_view()),
+    path("tutorial-steps/<int:pk>/", views.TutorialStepDetail.as_view()),
+    path("tutorial-attempts/", views.TutorialAttemptsList.as_view()),
+    path("tutorial-attempts/<int:pk>/", views.TutorialAttemptDetail.as_view()),
+    path("tutorial-feedbacks/", views.TutorialFeedbackList.as_view()),
+    path("tutorial-feedbacks/<int:pk>/", views.TutorialFeedbackDetail.as_view()),
 ]
