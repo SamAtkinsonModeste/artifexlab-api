2ad0f3c3e78a05e994be4edf341d9cceeee4801a
Sam Atkinson Modeste <sammodeste@yahoo.com>
2025-06-01 16:33:15 +0100

    feat:admin - registered missing models

--- comments/admin.py
+++ comments/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import CommentArtwork, CommentTutorial
+from .models import CommentArtwork, CommentTutorial, CommentTutorialAttempts
 
 # Register your models here.
 admin.site.register(CommentArtwork)
 admin.site.register(CommentTutorial)
+admin.site.register(CommentTutorialAttempts)
