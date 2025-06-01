04e50703c650d565e181a813349455540ac7c8ef
Sam Atkinson Modeste <sammodeste@yahoo.com>
2025-06-01 16:33:35 +0100

    feat:admin - registered missing models

--- likes/admin.py
+++ likes/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import LikeArtwork, LikeTutorial
+from .models import LikeArtwork, LikeTutorial, LikeTutorialAttempt
 
 #Register your models here.
 admin.site.register(LikeArtwork)
 admin.site.register(LikeTutorial)
+admin.site.register(LikeTutorialAttempt)
