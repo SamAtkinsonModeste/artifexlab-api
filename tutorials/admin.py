45912944551795bc948ba8f26e5138042251787f
Sam Atkinson Modeste <sammodeste@yahoo.com>
2025-06-01 16:33:48 +0100

    feat:admin - registered missing models

--- tutorials/admin.py
+++ tutorials/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import Tutorial
+from .models import Tutorial, TutorialSteps, TutorialAttempts, TutorialFeedback
 
 # Register your models here.
 admin.site.register(Tutorial)
+admin.site.register(TutorialSteps)
+admin.site.register(TutorialAttempts)
+admin.site.register(TutorialFeedback)
