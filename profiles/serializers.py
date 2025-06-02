75fc409bb1d363ff983d93f59f212ba28b545149
Sam Atkinson Modeste <sammodeste@yahoo.com>
2025-06-02 06:01:21 +0100

    test: comment out request-dependent fields in ProfileSerializer

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -5,27 +5,27 @@ from followers.models import Follower
 
 class ProfileSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source="owner.username")
-    is_owner = serializers.SerializerMethodField()
-    following_id = serializers.SerializerMethodField()
+    # is_owner = serializers.SerializerMethodField()
+    # following_id = serializers.SerializerMethodField()
     artworks_count = serializers.ReadOnlyField()
     tutorials_count = serializers.ReadOnlyField()
     tutorials_attempt_count = serializers.ReadOnlyField()
     followers_count = serializers.ReadOnlyField()
     following_count = serializers.ReadOnlyField()
 
-    def get_is_owner(self, obj):
-        request = self.context["request"]
-        return request.user == obj.owner
+    # def get_is_owner(self, obj):
+    #     request = self.context["request"]
+    #     return request.user == obj.owner
 
-    def get_following_id(self, obj):
-        user = self.context["request"].user
-        if user.is_authenticated:
-            following = Follower.objects.filter(
-                owner=user, followed=obj.owner
-            ).first()
-            print(following)
-            return following.id if following else None
-        return None
+    # def get_following_id(self, obj):
+    #     user = self.context["request"].user
+    #     if user.is_authenticated:
+    #         following = Follower.objects.filter(
+    #             owner=user, followed=obj.owner
+    #         ).first()
+    #         print(following)
+    #         return following.id if following else None
+    #     return None
 
     class Meta:
         model = Profile
@@ -37,8 +37,8 @@ class ProfileSerializer(serializers.ModelSerializer):
             "display_name",
             "bio",
             "profile_image",
-            "is_owner",
-            "following_id",
+            # "is_owner",
+            # "following_id",
             "artworks_count",
             "tutorials_count",
             "tutorials_attempt_count",
