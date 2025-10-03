385ffd0007e0600469c4604d5541b8e4de1c145b
Sam Atkinson Modeste <sammodeste@yahoo.com>
2025-10-03 14:50:51 +0100

    refactor(urls): likes - whitespace added

--- likes/urls.py
+++ likes/urls.py
@@ -4,10 +4,8 @@ from likes import views
 urlpatterns = [
     path("artwork-likes/", views.LikeArtworkList.as_view()),
     path("artwork-likes/<int:pk>/", views.LikeArtworkDetail.as_view()),
-    path("tutorial-likes/",views.LikeTutorialList.as_view()),
+    path("tutorial-likes/", views.LikeTutorialList.as_view()),
     path("tutorial-likes/<int:pk>/", views.LikeTutorialDetail.as_view()),
-    path("tutorial-attempt-likes/",views.LikeTutorialAttemptList.as_view()),
+    path("tutorial-attempt-likes/", views.LikeTutorialAttemptList.as_view()),
     path("tutorial-likes/<int:pk>/", views.LikeTutorialAttemptDetail.as_view()),
-
-
 ]
